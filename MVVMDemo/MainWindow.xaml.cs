b1be281880899e40b5bc4f780d80bcfecfb68082
Samuel <39803157+samuelbrasileiro@users.noreply.github.com>
2019-10-24 17:13:47 -0300

    Interface

---
+++ MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Microsoft.Kinect;
+
+namespace MVVMDemo
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+
+            
+
+            // StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
